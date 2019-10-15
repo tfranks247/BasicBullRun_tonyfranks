a60d12cccab8eccb3e7cda2176b5707ad5b9c89f
TPCF241005UR <TPCF241005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-15 16:53:38 -0400

    first push

---
+++ src/main/java/com/example/demo/Messages.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Messages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1)
+    private String content;
+
+    @NotNull
+    @Size(min=5)
+    private String date;
+
+    @NotNull
+    @Size(min=1)
+    private String sentBy;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+}
